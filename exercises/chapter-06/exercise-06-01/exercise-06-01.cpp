1c48407f84affaf7070f39b7d7cef25ca73c8ec7
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2022-03-21 07:42:12 +0100

    Add inital version of solution for exercise 6.1.

---
+++ exercises/chapter-06/exercise-06-01/exercise-06-01.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file exercise-06-01.cpp
+ * @author Henrik Samuelsson (henrik.samuelsson@gmail.com)
+ * @brief Exercise 6.1 Hello World and Comments from the book Modern C++ for
+ *        Absolute Beginners
+ * @version 0.1
+ * @date 2022-03-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+/**
+ * @brief Program that has a comment in it, outputs "Hello World." on one line,
+ *        and "C++ rocks!" on a new line.
+ *
+ * @return int 
+ */
+int main()
+{
+    // Below lines will output text in the console.
+    std::cout << "Hello World" << '\n';
+    std::cout << "C++ rocks!";
+}
