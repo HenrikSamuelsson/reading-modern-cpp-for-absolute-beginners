fcece52740cb444e1eb2d6aaed0a2d14af9e2dbd
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2022-03-21 14:09:34 +0100

    Revised comments.

--- exercises/chapter-06/exercise-06-01/exercise-06-01.cpp
+++ exercises/chapter-06/exercise-06-01/exercise-06-01.cpp
@@ -1,26 +1,26 @@
 /**
  * @file exercise-06-01.cpp
  * @author Henrik Samuelsson (henrik.samuelsson@gmail.com)
- * @brief Exercise 6.1 Hello World and Comments from the book Modern C++ for
- *        Absolute Beginners
- * @version 0.1
- * @date 2022-03-21
- * 
+ * @brief Exercise 6.1 Hello World and Comments
+ *
+ * Exercise solution for exercise in the book Modern C++ for Absolute Beginners.
+ *
+ * Program that has a comment in it, outputs "Hello World." on one line, and
+ * "C++ rocks!" on a new line.
+ *
  * @copyright Copyright (c) 2022
- * 
  */
 
 #include <iostream>
 
 /**
- * @brief Program that has a comment in it, outputs "Hello World." on one line,
- *        and "C++ rocks!" on a new line.
+ * @brief Outputs messages to the console.
  *
  * @return int 
  */
 int main()
 {
-    // Below lines will output text in the console.
+    // Below lines will output text in the console using cout.
     std::cout << "Hello World" << '\n';
     std::cout << "C++ rocks!";
 }
