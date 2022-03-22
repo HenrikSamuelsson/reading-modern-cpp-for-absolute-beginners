08736589ec77cd11e1ac79c4710ab621ce70a2f9
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2022-03-22 07:59:33 +0100

    Add notes on things learned from exercise 6.1.

--- exercises/chapter-06/exercise-06-01/exercise-06-01.cpp
+++ exercises/chapter-06/exercise-06-01/exercise-06-01.cpp
@@ -1,20 +1,27 @@
 /**
  * @file exercise-06-01.cpp
+ * @copyright Copyright (c) 2022
  * @author Henrik Samuelsson (henrik.samuelsson@gmail.com)
  * @brief Exercise 6.1 Hello World and Comments
  *
- * Exercise solution for exercise in the book Modern C++ for Absolute Beginners.
+ * Solution for exercise 6.1 in the book Modern C++ for Absolute Beginners.
  *
  * Program that has a comment in it, outputs "Hello World." on one line, and
  * "C++ rocks!" on a new line.
  *
- * @copyright Copyright (c) 2022
+ * Key takeaways from this exercise:
+ *  - The code in the function main is the execution start point of C++ program
+ *  - A single line comment starts with // and ends when the line ends
+ *  - Text can be output to the console using std::cout and the operator <<
+ *  - A backslash and the letter n causes a line break in console output
+ *  - Statements in C++ shall end with a semicolon
+ *
  */
 
 #include <iostream>
 
 /**
- * @brief Outputs messages to the console.
+ * @brief Outputs two messages to the console.
  *
  * @return int 
  */
