286d7875ecf2b298a7e33edc3d88c7845d8a6a10
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-03-23 08:03:27 +0100

    Update comments.

--- exercises/chapter-06/exercise-06-01/exercise-06-01.cpp
+++ exercises/chapter-06/exercise-06-01/exercise-06-01.cpp
@@ -1,16 +1,16 @@
 /**
  * @file        exercise-06-01.cpp
- * @copyright   Copyright (c) 2022
  * @author      Henrik Samuelsson (henrik.samuelsson@gmail.com)
  * @brief       Exercise 6.1 Hello World and Comments
  *
- * Solution for exercise 6.1 in the book Modern C++ for Absolute Beginners.
+ * Solution for exercise 6.1 from the book Modern C++ for Absolute Beginners.
  *
  * Program that has a comment in it, outputs "Hello World." on one line, and
  * "C++ rocks!" on a new line.
  *
  * Key takeaways from this exercise:
- *  - The code in the function main is the execution start point of C++ program
+ *  - The library file iostream need to be included if to use std::cout
+ *  - The function main is the execution start point of a C++ program
  *  - A single line comment starts with // and ends when the line ends
  *  - Text can be output to the console using std::cout and the operator <<
  *  - A backslash and the letter n causes a line break in console output
