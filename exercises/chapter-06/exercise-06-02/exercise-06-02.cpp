48dcd178e9dab14c36cc99e574acf3229828e0c6
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-03-23 08:06:10 +0100

    Add solution to exercise 6.2.

---
+++ exercises/chapter-06/exercise-06-02/exercise-06-02.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file        exercise-06-02.cpp
+ * @author      Henrik Samuelsson (henrik.samuelsson@gmail.com)
+ * @brief       Exercise 6.2 Declaration
+ *
+ * Solution for exercise 6.2 from the book Modern C++ for Absolute Beginners.
+ *
+ * Program that declares three variables inside the main function.
+ *
+ * Key takeaways from this exercise:
+ *  - A declaration introduces a name for a variable into a certain scope
+ *  - A variable can be used first after it have been declared
+ *  - A declaration shall start with the type of the variable
+ *  - The name of a variable shall be placed after the type in the declaration
+ */
+
+/**
+ * @brief   Declares three variables of various types.
+ */
+int main()
+{
+    char myChar;
+    int myInt;
+    double myDouble;
+}
