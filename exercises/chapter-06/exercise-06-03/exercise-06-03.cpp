75534f9f046a8cf9278bcc5b378e8e2ac79857b0
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-03-25 07:35:48 +0100

    Add solution to exercise 6.3.

---
+++ exercises/chapter-06/exercise-06-03/exercise-06-03.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file        exercise-06-03.cpp
+ * @author      Henrik Samuelsson (henrik.samuelsson@gmail.com)
+ * @brief       Exercise 6.3 Definition
+ *
+ * Solution for exercise 6.3 from the book Modern C++ for Absolute Beginners.
+ *
+ * Program that defines three variables inside the main function.
+ *
+ * Key takeaways from this exercise:
+ *  - A definition is a declaration and a initialization of a variable
+ *  - A definition will cause a memory location to be reserved for the variable
+ *  - All definitions are declarations but not all declarations are definitons
+ */
+
+/**
+ * @brief   Defines three variables of various types.
+ */
+int main()
+{
+    char c = 'a';
+    int i = 42;
+    double d = 0.5; 
+}
