411002cd7ef67e66070473bc1c8245ae146fe01f
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-03-26 09:33:50 +0100

    Use new comment formatting.

--- exercises/chapter-06/exercise-06-03/exercise-06-03.cpp
+++ exercises/chapter-06/exercise-06-03/exercise-06-03.cpp
@@ -1,10 +1,11 @@
 /**
- * @file        exercise-06-03.cpp
- * @author      Henrik Samuelsson (henrik.samuelsson@gmail.com)
- * @brief       Exercise 6.3 Definition
+ * @copyright Copyright 2022 Henrik Samuelsson
+ * @file      exercise-06-03.cpp
+ * @author    Henrik Samuelsson (henrik.samuelsson@gmail.com)
+ * @brief     Exercise 6.3 Definition
  *
  * Solution for exercise 6.3 from the book Modern C++ for Absolute Beginners.
- *
+ * 
  * Program that defines three variables inside the main function.
  *
  * Key takeaways from this exercise:
@@ -12,6 +13,8 @@
  *  - A definition will cause a memory location to be reserved for the variable
  *  - All definitions are declarations but not all declarations are definitons
  */
+
+#include <iostream>
 
 /**
  * @brief   Defines three variables of various types.
@@ -21,4 +24,8 @@ int main()
     char c = 'a';
     int i = 42;
     double d = 0.5;
+
+    std::cout << "The value of a char variable is: " << c << '\n';
+    std::cout << "The value of an int variable is: " << i << '\n';
+    std::cout << "The value of a double variable is: " << d << '\n';
 }
