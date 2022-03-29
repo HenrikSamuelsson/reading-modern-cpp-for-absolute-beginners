23f7f3fe556966d2413c50adb8c5bf11f92237d2
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-03-29 07:46:44 +0200

    Add solution to exercise 24.1.

---
+++ exercises/chapter-24/exercise-24-01/exercise-24-01.cpp
@@ -0,0 +1,27 @@
+/**
+ * @copyright Copyright 2022 Henrik Samuelsson
+ * @file      exercise-09-01.cpp
+ * @author    Henrik Samuelsson (henrik.samuelsson@gmail.com)
+ * @brief     Exercise 24.1 Class Instance
+ *
+ * Solution for exercise 24.1 from the book Modern C++ for Absolute Beginners.
+ *
+ * A program that defines an empty class called MyClass and makes an instance of
+ * MyClass in the main function.
+ *
+ * Key takeaways from this exercise:
+ *  - The keyword class is used when defining a class
+ *  - Class names will conventionally start with an uppercase letter
+ *  - A class definition shall have a semicolon at the end
+ *  - An instance is made using the classname followed by a chosen instance name
+ */
+
+class MyClass
+{
+
+};
+
+int main()
+{
+    MyClass o;
+}
