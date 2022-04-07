c71dde581ba719bc5d7313c894a9fa78c5a0b77d
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-04-07 08:09:37 +0200

    Add start of exercise 15.5.

---
+++ exercises/chapter-15/exercise-15-05/exercise-15-05.cpp
@@ -0,0 +1,6 @@
+/**
+ * @copyright Copyright 2022 Henrik Samuelsson
+ * @file      exercise-15-05.cpp
+ * @author    Henrik Samuelsson (henrik.samuelsson@gmail.com)
+ * @brief     Exercise 15.5 Strings from Standard Input
+ */
