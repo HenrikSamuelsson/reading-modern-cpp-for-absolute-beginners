df50edfe21946e7d1f6824571a5c2ef97ba6622d
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-04-08 13:24:12 +0200

    Add documentation.

--- exercises/chapter-15/exercise-15-06/exercise-15-07.cpp
+++ exercises/chapter-15/exercise-15-06/exercise-15-07.cpp
@@ -1,5 +1,15 @@
+/**
+ * @copyright Copyright 2022 Henrik Samuelsson
+ * @file      exercise-15-07.cpp
+ * @author    Henrik Samuelsson (henrik.samuelsson@gmail.com)
+ * @brief     Exercise 15.6 Creating a Substring
+ */
+
 #include <iostream>
 
+/**
+ * @brief     Splits a string into substrings based on hardcoded lengths.
+ */
 int main()
 {
     std::string fullname = "Henrik Samuelsson";
