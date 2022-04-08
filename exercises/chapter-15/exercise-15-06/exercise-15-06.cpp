a1555db405582f647849ebed7915411490236165
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-04-08 13:50:56 +0200

    A notes about things learned from the exercise.

--- exercises/chapter-15/exercise-15-06/exercise-15-06.cpp
+++ exercises/chapter-15/exercise-15-06/exercise-15-06.cpp
@@ -1,8 +1,17 @@
 /**
  * @copyright Copyright 2022 Henrik Samuelsson
- * @file      exercise-15-07.cpp
+ * @file      exercise-15-06.cpp
  * @author    Henrik Samuelsson (henrik.samuelsson@gmail.com)
  * @brief     Exercise 15.6 Creating a Substring
+ * 
+ * Solution for exercise 15.6 from the book Modern C++ for Absolute Beginners.
+ * 
+ * Program that creates two substrings from a parent string.
+ * 
+ * Key takeaways from this exercise:
+ *  - The string library member function substr copies a part of a string
+ *  - First argument to substr is the start position in the original string
+ *  - Second argument to substr is the number of characters to copy
  */
 
 #include <iostream>
