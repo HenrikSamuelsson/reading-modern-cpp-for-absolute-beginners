387870972637379600ecfc4db22b77e1420bf376
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-04-12 12:42:43 +0200

    Add inital version of solution for exercise 18.1.

---
+++ exercises/chapter-18/exercise-18-01/exercise-18-01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main()
+{
+    bool isRaining = false;
+    if (isRaining)
+    {
+        std::cout << "It is raining, bring an umbrella." << '\n';
+    }
+    else
+    {
+        std::cout << "It is not raining, no need to bring an umbrella." << '\n';
+    }
+}
