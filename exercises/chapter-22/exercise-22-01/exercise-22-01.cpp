81a164ea6dd535571780398b92240ce2e1c14b7a
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2022-04-25 07:44:27 +0200

    Add comments on allocation and deallocation.

--- exercises/chapter-22/exercise-22-01/exercise-22-01.cpp
+++ exercises/chapter-22/exercise-22-01/exercise-22-01.cpp
@@ -2,8 +2,10 @@
 
 int main()
 {
+    // The storage for x and y is allocated here and is placed on the stack.
     int x = 123;
     int y = 456;
     std::cout << "The values with automatic storage durations are: " << x
               << " and " << y;
+    // The storage for x and y is deallocated here at the end of the code block.
 }
